7326511202a62d0008ec66202a18d54b930f1444
FatemaAlbalushi <fatoomsaid188@gmail.com>
2022-12-07 21:28:41 -0800

    Welcome to School System

---
+++ src/School.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAP-10
+ *
+ */
+public class School {
+	
+		public static void main(String[] args){
+		System.out.println("Wellcome to School Management System ");
+		}
+		
+	
+
+}
